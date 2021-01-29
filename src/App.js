8796221d5296f01efe49aaeb6177ef13754b6854
ramagg <rg1998@hotmail.es>
2021-01-28 23:12:35 -0300

    working version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+
+import './Game.css'
 
 function App() {
+  const [ttt, setTTT] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0])
+  const [gamestart, setGameStart] = useState(false)
+  const [player, setPlayer] = useState(false)
+  const [lastWon, setLastWon] = useState('-')
+  const [moves, setMoves] = useState(0)
+
+  const checkWin = () => {
+    const checks = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ]
+    let flag = false
+    checks.forEach((pos) => {
+      if (ttt[pos[0]] === ttt[pos[1]] && ttt[pos[1]] === ttt[pos[2]]) {
+        if (!(ttt[pos[0]] === 0)) {
+          flag = true
+        }
+      }
+    })
+    if (flag) {
+      setTTT([...[0, 0, 0, 0, 0, 0, 0, 0, 0]])
+      setLastWon(player === true ? 'X' : 'O')
+      setGameStart(false)
+      setMoves(0)
+    }
+    if (moves === 9) {
+      setTTT([...[0, 0, 0, 0, 0, 0, 0, 0, 0]])
+      setLastWon('-')
+      setGameStart(false)
+      setMoves(0)
+    }
+  }
+
+  const pick = (index) => {
+    // verify if already selected...
+    setGameStart(true)
+    if (ttt[index] === 0) {
+      ttt[index] = player === false ? 1 : 2
+      setTTT([...ttt])
+      setPlayer(!player)
+      setMoves(moves + 1)
+    }
+    // console.log(ttt)
+  }
+
+  useEffect(() => {
+    if (gamestart) {
+      checkWin()
+    }
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="Game">
+        {ttt.map((position, i) => {
+          let text = '_'
+          switch (position) {
+            case 0:
+              text = '_'
+              break
+            case 1:
+              text = 'X'
+              break
+            case 2:
+              text = 'O'
+              break
+          }
+          return (
+            <div className="blk" key={i} onClick={() => pick(i)}>
+              {text}
+            </div>
+          )
+        })}
+      </div>
+      <div className="hud">
+        <p>Current player: {player === false ? 'X' : 'O'}</p>
+        <p>Last Won: {lastWon}</p>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
